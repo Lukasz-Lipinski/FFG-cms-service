f1441bbde6aab620384e79e21f6ae2261cd43b21
Łukasz Lipiński <74236310+Lukasz-Lipinski@users.noreply.github.com>
2026-05-07 12:20:00 +0200

    About section (#3)
    * about section done
    
    * vercel config added
    
    * fixing vercel config
    
    * fixed config

--- src/main.ts
+++ src/main.ts
@@ -5,15 +5,13 @@ import { AppDataSource } from './core/database/db';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  console.log('Server is running on: ', process.env.PORT ?? 3000);
-
-  try {
-    await AppDataSource.initialize();
-    console.log('Data Source has been initialized!');
-  } catch (error) {
-    console.error('Error during Data Source initialization', error);
-  }
 
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+try {
+  AppDataSource.initialize();
+  console.log('Data Source has been initialized!');
+  bootstrap();
+} catch (error) {
+  console.error('Error during Data Source initialization', error);
+}
