c9b7789b1810fc7c8562d10ef16f32066c6d0236
Lukasz-Lipinski <l.lipinski1993@gmail.com>
2026-05-10 09:29:25 +0200

    config fixed

--- src/modules/discography/controllers/discography.controller.ts
+++ src/modules/discography/controllers/discography.controller.ts
@@ -1,5 +1,5 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
-import { AddNewAlbumDto } from './dto/AddNewAlbumDto';
+import { AddNewAlbumDto } from '../dto/AddNewAlbumDto';
 
 @Controller('discography')
 export class DiscographyController {
